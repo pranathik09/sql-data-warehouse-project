e7f98b67b3a1aecf1d14193dec7410ddd0cd8f76
pranathik09 <karumudi.pranathi09@gmail.com>
2025-09-18 22:05:33 -0500

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,4 +1,13 @@
-
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 IF OBJECT_ID ('blayer.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE blayer.crm_cust_info;
 CREATE TABLE blayer.crm_cust_info(

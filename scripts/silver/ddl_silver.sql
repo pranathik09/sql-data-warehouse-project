680adeb8879936b7542154bf2b7fc24019a6137d
pranathik09 <karumudi.pranathi09@gmail.com>
2025-09-18 19:33:30 -0500

    Create ddl_silver.sql

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,65 @@
+
+IF OBJECT_ID ('slayer.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE slayer.crm_cust_info;
+CREATE TABLE slayer.crm_cust_info(
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_materialstatus NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE
+);
+
+IF OBJECT_ID ('slayer.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE slayer.crm_prd_info;
+CREATE TABLE slayer.crm_prd_info(
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
+);
+
+IF OBJECT_ID ('slayer.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE slayer.crm_sales_details;
+CREATE TABLE slayer.crm_sales_details(
+	sls_order_num NVARCHAR(50),
+	sls_product_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+);
+
+IF OBJECT_ID ('slayer.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE slayer.erp_loc_a101;
+
+CREATE TABLE slayer.erp_loc_a101(
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
+);
+
+IF OBJECT_ID ('slayer.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE slayer.erp_cust_az12;
+
+CREATE TABLE slayer.erp_cust_az12(
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50)
+);
+
+IF OBJECT_ID ('slayer.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE slayer.erp_px_cat_g1v2;
+
+CREATE TABLE slayer.erp_px_cat_g1v2(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
+);
